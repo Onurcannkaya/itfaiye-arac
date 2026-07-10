8d52c0cf0191d1a9feea65ab48fbf238110dcca4
Onur <kayaonurcan58@gmail.com>
2026-07-10 16:54:48 +0300

    fix: restore sprinter yOffset back to -0.85

--- src/components/vehicle/Vehicle3DGarage.tsx
+++ src/components/vehicle/Vehicle3DGarage.tsx
@@ -90,7 +90,7 @@ const VEHICLE_MODEL_CONFIGS: Record<string, {
   default: { targetLength: 6.0, envMapIntensity: 1.5 },
   fiat_doblo: { targetLength: 4.5, envMapIntensity: 2.0, yOffset: -0.02 },
   hyundai_accent: { targetLength: 4.3, envMapIntensity: 1.5, yOffset: -0.02 },
-  sprinter: { targetLength: 5.8, envMapIntensity: 1.5, yOffset: -0.05 },
+  sprinter: { targetLength: 5.8, envMapIntensity: 1.5, yOffset: -0.85 },
   merdiven: { targetLength: 10.0, envMapIntensity: 1.5, rotationY: -Math.PI / 2, yOffset: 0.01 },
 }
 
